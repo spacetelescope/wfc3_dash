79cb7252beeb14875cc3406570be2764ba5cabfa
Catherine A. Martlin <cmartlin@stsci.edu>
2018-10-04 16:53:12 -0400

    Updates to indents and opening file.

--- wfc3_dash/reduce_dash.py
+++ wfc3_dash/reduce_dash.py
@@ -69,22 +69,22 @@ class DashData(object):
         ### If it does - test it's content
 
         ###### Test whether it is a wfc3/ir image
-        if ~( (INSTRUME == 'WFC3  ') & (DETECTOR == 'IR  ') ):
-            raise Exception('This observation was not performed with WFC3/IR')
+            if ~( (self.ima_file[0].header['INSTRUME'] == 'WFC3  ') & (self.ima_file[0].header['DETECTOR'] == 'IR  ') ):
+                raise Exception('This observation was not performed with WFC3/IR')
 
-        ###### Test whether it has more than one science extension (FLTs have only 1)
-            nsci = [ext.header['EXTNAME '] for ext in self.ima_file[1:]].count('SCI')
-        if nsci == 1:
-            raise Exception('This file has only one science extension, it cannot be a WFC3/IR ima')
+            ###### Test whether it has more than one science extension (FLTs have only 1)
+                nsci = [ext.header['EXTNAME '] for ext in self.ima_file[1:]].count('SCI')
+            if nsci == 1:
+                raise Exception('This file has only one science extension, it cannot be a WFC3/IR ima')
 
 
-        calib_keys = ['DQICORR','ZSIGCORR','ZOFFCORR','DARKCORR','BLEVCORR','NLINCORR','FLATCORR','CRCORR','UNITCORR','PHOTCORR','RPTCORR','DRIZCORR']
-        performed = 0
-        for ck in calib_keys:
-            if self.ima_file[ck] == 'COMPLETE':
-            performed = performed + 1
-        if performed == 0:
-            raise Exception('This file looks like a RAW file')     
+            calib_keys = ['DQICORR','ZSIGCORR','ZOFFCORR','DARKCORR','BLEVCORR','NLINCORR','FLATCORR','CRCORR','UNITCORR','PHOTCORR','RPTCORR','DRIZCORR']
+            performed = 0
+            for ck in calib_keys:
+                if self.ima_file[ck] == 'COMPLETE':
+                performed = performed + 1
+            if performed == 0:
+                raise Exception('This file looks like a RAW file')     
 
             except IOError:
                 print('Cannot read file.')
